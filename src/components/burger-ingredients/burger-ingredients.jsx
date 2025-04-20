b4136090f83cccc5f602f37b3e52e50389c37aca
mkosikov <mJdwOVeBV4pW>
2025-04-20 14:54:59 +0300

    Add burger constructor

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Data } from '../../utils/data'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import IngredientCategory from './ingredient-category/ingredient-category';
 import styles from './burger-ingredients.module.css';
 
-const BurgerIngredients = () => {
+function SelectIngredientCategory(current, data){
+    switch(current){
+      case "mains": return <IngredientCategory title="Начинки" elements={data.filter(r => r.type === "main")}/>;
+      case "sauces": return <IngredientCategory title="Соусы"  elements={data.filter(r => r.type === "sauce")}/>;
+      default: return <IngredientCategory title="Булки" elements={data.filter(r => r.type === "bun")}/>;
+    }
+}
+
+const BurgerIngredients = ( { data } ) => {
   const [current, setCurrent] = React.useState('buns');
   console.log(current);
 
   return (
     <section className={styles.container}>
-      
-      <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <div className={styles.tabs}>
         <Tab value="buns" active={current === 'buns'} onClick={setCurrent}>
           Булки
@@ -25,9 +30,7 @@ const BurgerIngredients = () => {
       </div>
 
       <div className={styles.scrollable}>
-        <IngredientCategory title="Булки" elements={Data.filter(r => r.type === "bun")}/>
-        <IngredientCategory title="Соусы"  elements={Data.filter(r => r.type === "sauce")} />
-        <IngredientCategory title="Начинки" elements={Data.filter(r => r.type === "main")} />
+        {SelectIngredientCategory(current, data)}
       </div>
     </section>
   );
